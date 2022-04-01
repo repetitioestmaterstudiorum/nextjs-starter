097623dae5f3c9af4d1d1d8987d4aa5de560667c
repetitioestmaterstudiorum <thotan@ik.me>
2022-04-01 11:46:18 +0200

    fix live url when in production env

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const isProduction = process.env.NODE_ENV === 'production'
 
-export const server = isProduction ? '...' : 'http://localhost:3000'
+export const server = isProduction ? process.env.LIVE_URL : 'http://localhost:3000'
